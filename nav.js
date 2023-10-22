4164cf096431a4740f713666abaa64051644e42d
Durgadevi1212 <durga1212devi@gmail.com>
2023-10-22 09:41:05 +0530

    files added

---
+++ nav.js
@@ -0,0 +1,117 @@
+const form = document.querySelector('#form')
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const cpassword = document.querySelector('#cpassword');
+var emailArr =[]
+var passwordArr = []
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    
+    if(!validateInputs()){
+      
+        alert("you have been successfully sign up")
+        window.location.href='index.html'
+    }
+})
+
+function validateInputs(){
+    const usernameVal = username.value.trim()
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+    const cpasswordVal = cpassword.value.trim();
+    let success = true
+
+    if(usernameVal===''){
+        success=false;
+        setError(username,'Username is required')
+    }
+    else{
+        setSuccess(username)
+    }
+
+    if(emailVal===''){
+        success = false;
+        setError(email,'Email is required')
+    }
+    else if(!validateEmail(emailVal)){
+        success = false; 
+        setError(email,'Please enter a valid email')
+    }
+    else{
+        setSuccess(email)
+    }
+
+    if(passwordVal === ''){
+        success= false;
+        setError(password,'Password is required')
+    }
+    else if(passwordVal.length<8){
+        success = false;
+        setError(password,'Password must be atleast 8 characters long')
+    }
+    else{
+        setSuccess(password)
+    }
+
+    if(cpasswordVal === ''){
+        success = false;
+        setError(cpassword,'Confirm password is required')
+    }
+    else if(cpasswordVal!==passwordVal){
+        success = false;
+        setError(cpassword,'Password does not match')
+    }
+    
+ if(success){
+        setData(emailVal,cpasswordVal)
+    }
+    else{
+         return success;
+    }
+
+    return success;
+
+}
+
+function setError(element,message){
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector('.error')
+
+    errorElement.innerText = message;
+    inputGroup.classList.add('error')
+    inputGroup.classList.remove('success')
+}
+
+function setSuccess(element){
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector('.error')
+
+    errorElement.innerText = '';
+    inputGroup.classList.add('success')
+    inputGroup.classList.remove('error')
+}
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+  
+  function setData(email, password){
+
+    let existingEmailData = localStorage.getItem('Email')
+    let emailVal = existingEmailData ? JSON.parse(existingEmailData) : []
+    emailVal.push(email)
+    localStorage.setItem('Email', JSON.stringify(emailVal))
+
+    let existingPasswordData = localStorage.getItem('Password')
+    let passwordVal = existingPasswordData ? JSON.parse(existingPasswordData) : []
+    passwordVal.push(password)
+    localStorage.setItem('Password', JSON.stringify(passwordVal))
+
+}
